2c07312aa958e4cb7aa198bf226c40acb3f7345a
nicocastx <nicocastx@gmail.com>
2025-08-26 11:12:06 -0300

    version 0.3.1 - arreglos menores y comentarios exceptionAdvice

--- msvccursos/src/main/java/com/udemyjavamicro/springcloud/msvccursos/controllers/exceptionAdvice/ExceptionControllerAdvice.java
+++ msvccursos/src/main/java/com/udemyjavamicro/springcloud/msvccursos/controllers/exceptionAdvice/ExceptionControllerAdvice.java
@@ -3,68 +3,69 @@ package com.udemyjavamicro.springcloud.msvccursos.controllers.exceptionAdvice;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.View;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ExceptionControllerAdvice  {
 
-    private final View error;
+    private final HashMap<String, Object> error;
 
-    public ExceptionControllerAdvice(View error) {
+    public ExceptionControllerAdvice(HashMap<String, Object> error) {
         this.error = error;
     }
 
-    //no se encontro el elemento
+    //Error en una busqueda
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<?> noElement(NoSuchElementException e, HttpServletRequest request) {
         int statusCode = HttpStatus.NOT_FOUND.value();
-        HashMap<String, String> error = formatError(e, request, statusCode);
+        HashMap<String, Object> error = formatError(e, request, statusCode);
         error.put("statusCode:", String.valueOf(statusCode));
         return ResponseEntity.status(statusCode).body(error);
     }
 
-    //metodo no soportado por la aplicacion
+    //Endpoint y/o metodo invalido
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<?> methodNotSupported(HttpRequestMethodNotSupportedException e, HttpServletRequest request) {
         int statusCode = HttpStatus.METHOD_NOT_ALLOWED.value();
-        HashMap<String, String> error = formatError(e, request, statusCode);
+        HashMap<String, Object> error = formatError(e, request, statusCode);
         error.put("statusCode:", String.valueOf(HttpStatus.METHOD_NOT_ALLOWED.value()));
         return ResponseEntity.status(statusCode).body(error);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> exception(Exception e, HttpServletRequest request) {
-        int statusCode = HttpStatus.INTERNAL_SERVER_ERROR.value();
-        HashMap<String, String> error = formatError(e, request, statusCode);
-        return ResponseEntity.status(statusCode).body(error);
-    }
-
+    //Spring Validation
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
         int statusCode = HttpStatus.BAD_REQUEST.value();
-        String validations = e.getBindingResult()
+        List<String> validations = e.getBindingResult()
                 .getFieldErrors()
                 .stream()
                 .map(invalid -> invalid.getField() + " : " + invalid.getDefaultMessage())
-                .collect(Collectors.joining("\n"));
-        HashMap<String, String> error = formatError(e, request, statusCode);
+                .toList();
+        HashMap<String, Object> error = formatError(e, request, statusCode);
         error.put("validations:", validations);
-
+        
         return ResponseEntity.status(statusCode).body(error);
     }
 
-    //errores generales capturados
-    private HashMap<String, String> formatError(Exception e, HttpServletRequest request, int statusCode){
-        HashMap<String, String> error = new HashMap<>();
+
+    //Error general
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> exception(Exception e, HttpServletRequest request) {
+        int statusCode = HttpStatus.INTERNAL_SERVER_ERROR.value();
+        HashMap<String, Object> error = formatError(e, request, statusCode);
+        return ResponseEntity.status(statusCode).body(error);
+    }
+
+    //formato mensaje de error
+    private HashMap<String, Object> formatError(Exception e, HttpServletRequest request, int statusCode){
+        HashMap<String, Object> error = new HashMap<>();
         StackTraceElement description = e.getStackTrace()[0];
         error.put("statusCode:", String.valueOf(statusCode));
         error.put("message", e.getMessage().isBlank() ? "Error General" : e.getMessage());
